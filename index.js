ee1c5be0158607025459e99bf3779849436d3385
Zero <vinhphu458@gmail.com>
2018-01-12 23:13:46 +0700

    set up listen any ports

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express()
 
 app.set("view engine", "ejs")
 app.set("views", "./views")
-app.listen(8080)
+app.listen(process.env.PORT || 8080)
 app.get("/", function (req, res) {
     res.render("home")
     console.log("home page started")
