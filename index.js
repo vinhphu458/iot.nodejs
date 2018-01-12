0f4cbec179e6270974b1ed52d3d78fd80d2b2790
unknown <vinhphu458@gmail.com>
2018-01-12 23:04:27 +0700

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express")
+var app = express()
+
+app.set("view engine", "ejs")
+app.set("views", "./views")
+app.listen(8080)
+app.get("/", function (req, res) {
+    res.render("home")
+    console.log("home page started")
+})
